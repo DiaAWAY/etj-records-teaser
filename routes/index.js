6a5c4bedae35a31445dff7f9b7042956d9df47d0
DiaAWAY <sindrerh@hotmail.com>
2014-02-25 19:17:20 +0100

    some css fixes, large text on promotionals, added possibility for descriptions on promos

--- routes/index.js
+++ routes/index.js
@@ -2,23 +2,24 @@
  * GET home page.
  */
 
-function Album(band, title, url) {
+function Album(band, title, description, url) {
     this.band = band;
     this.title = title;
+    this.description = description;
     this.url = url;
 }
 
 // Dummy albums
 var albums = [
-    new Album('Container 90', 'Working Class League', 'container90_WorkingClassLeague.jpg'),
-    new Album('Container 90', 'World Champion Shit', 'container90_WorldChampionShit.jpg'),
-    new Album('Front 242', 'Tyranny (For You)', 'front242_TyrannyForYou.jpg'),
-    new Album('Front 242', 'Quite Unusual',  'front242_QuiteUnusual.jpg'),
-    new Album( 'Spark!', 'Hela Din Värld', 'spark_HelaDinVarld.jpg')
+    new Album('Container 90', 'Working Class League', 'description', 'container90_WorkingClassLeague.jpg'),
+    new Album('Container 90', 'World Champion Shit', 'description', 'container90_WorldChampionShit.jpg'),
+    new Album('Front 242', 'Tyranny (For You)', 'description', 'front242_TyrannyForYou.jpg'),
+    new Album('Front 242', 'Quite Unusual', 'description',  'front242_QuiteUnusual.jpg'),
+    new Album( 'Spark!', 'Hela Din Värld', 'description', 'spark_HelaDinVarld.jpg')
 ];
 
 var promotionals = [
-    new Album( 'White Trash Wankers', 'Album Title', 'WTW-Poster_small.jpg')
+    new Album( 'White Trash Wankers', 'Album Title', 'description', 'WTW-Poster_small.jpg')
 ];
 
 exports.index = function (req, res) {
